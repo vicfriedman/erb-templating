e848e62c03b139697b152990061ac17fc83e175e
Logan Hasson <logan.hasson@gmail.com>
2014-02-20 11:48:52 -0500

    Passing first site_generator_spec spec

---
+++ lib/site_generator.rb
@@ -0,0 +1,30 @@
+class SiteGenerator
+
+  def make_index!
+    movies = Movie.all
+    html = <<-HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Movies</title>
+  </head>
+  <body>
+    <ul>
+HTML
+    movies[0..-2].each do |movie|
+      html << "      <li><a href='movies/#{movie.title}'>#{movie.title}</a></li>\n"
+    end
+    html << "      <li><a href='movies/#{movies.last.title}'>#{movies.last.title}</a></li>"
+    html << <<-HTML
+
+    </ul>
+  </body>
+</html>
+HTML
+    File.open('_site/index.html', 'w') do |f|
+      f.write(html.chomp)
+      f.close
+    end
+  end
+
+end
