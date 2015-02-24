e0bf003ab149ed5bc14dcb563a5b0a0eb5714f2c
changamanda <amanda.chang10@gmail.com>
2015-02-24 13:36:34 -0500

    Refactor confusing solution

--- lib/site_generator.rb
+++ lib/site_generator.rb
@@ -3,24 +3,25 @@ class SiteGenerator
   def make_index!
     movies = Movie.all
     html = <<-HTML
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Movies</title>
-  </head>
-  <body>
-    <ul>
-HTML
-    movies[0..-2].each do |movie|
-      html << "      <li><a href=\"movies/#{movie.url}\">#{movie.title}</a></li>\n"
-    end
-    html << "      <li><a href=\"movies/#{movies.last.url}\">#{movies.last.title}</a></li>"
-    html << <<-HTML
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Movies</title>
+      </head>
+      <body>
+        <ul>
+    HTML
 
-    </ul>
-  </body>
-</html>
-HTML
+    movies.each do |movie|
+      html << "<li><a href=\"movies/#{movie.url}\">#{movie.title}</a></li>\n"
+    end
+
+    html << <<-HTML
+        </ul>
+      </body>
+    </html>
+    HTML
+
     File.open('_site/index.html', 'w') do |f|
       f.write(html.chomp)
       f.close
